7b822829f7fc6514e7020156afd6c6233c9d6000
SantaMinus <santaminus@gmail.com>
2021-03-07 19:07:34 +0200

    Remove unneeded LOG

--- src/main/java/com/sava/teamcitymonitor/TeamCityMonitorApplication.java
+++ src/main/java/com/sava/teamcitymonitor/TeamCityMonitorApplication.java
@@ -1,7 +1,5 @@
 package com.sava.teamcitymonitor;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,8 +7,6 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class TeamCityMonitorApplication extends SpringBootServletInitializer {
-    private static final Logger LOG = LoggerFactory.getLogger(TeamCityMonitorApplication.class);
-
     @Value("${teamcity.host}")
     String host;
 
