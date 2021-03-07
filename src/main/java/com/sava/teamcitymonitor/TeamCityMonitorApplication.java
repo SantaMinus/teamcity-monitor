5f271f88c3a595a6b6a3ba6785606090aa109979
SantaMinus <santaminus@gmail.com>
2021-03-07 22:25:16 +0200

    Stop extending SpringBootServletInitializer

--- src/main/java/com/sava/teamcitymonitor/TeamCityMonitorApplication.java
+++ src/main/java/com/sava/teamcitymonitor/TeamCityMonitorApplication.java
@@ -2,10 +2,9 @@ package com.sava.teamcitymonitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TeamCityMonitorApplication extends SpringBootServletInitializer {
+public class TeamCityMonitorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TeamCityMonitorApplication.class, args);
