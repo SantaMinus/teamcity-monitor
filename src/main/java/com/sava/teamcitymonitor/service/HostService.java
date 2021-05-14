e608be2660c5f43ec3ba74fcb884315b82eaab1a
Katia Savina <santaminus@gmail.com>
2021-05-14 11:10:53 +0300

    Introduce /host endpoints

---
+++ src/main/java/com/sava/teamcitymonitor/service/HostService.java
@@ -0,0 +1,12 @@
+package com.sava.teamcitymonitor.service;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+
+@Service
+@Getter
+@Setter
+public class HostService {
+    private String host;
+}
